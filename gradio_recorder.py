5f6907d42f9e0468ab3da64ed2dfddb901033300
ronjoy-databricks <ron.joy@databricks.com>
2024-04-10 14:39:14 -0500

    simple code to get calendar events

--- gradio_recorder.py
+++ gradio_recorder.py
@@ -2,6 +2,7 @@ import gradio as gr
 from transformers import pipeline
 import numpy as np
 
+from calendar_dates import get_all_events
 from summarize_to_doc import meeting_minutes
 
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
@@ -15,10 +16,13 @@ def transcribe(audio):
     return meeting_minutes(audio_text)
 
 
-demo = gr.Interface(
-    transcribe,
-    gr.Audio(sources=["microphone"]),
-    "text",
+iface = gr.Interface(
+    fn=transcribe,
+    inputs=[gr.Audio(sources=["microphone"]), gr.Dropdown(label="Select Upcoming Meeting", choices=[event['summary'] for event in get_all_events()])],
+    outputs="text",
+    title="Transcribe and Associate with Meeting",
+    description="Transcribe audio and select an upcoming meeting to associate with the transcription."
+    # style=gr.styles.Card,
 )
 
-demo.launch()
+iface.launch()
